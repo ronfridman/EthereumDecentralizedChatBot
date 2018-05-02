aed83e06e8422eedf0327122afdc4910b9ad02a9
Ron Fridman <fridmanron@gmail.com>
2018-05-02 16:32:38 +0300

    express server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var serveStatic = require('serve-static')
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use('*', express.static('index.html'))
+var port = process.env.PORT || 5000
+
+app.listen(port)
+
+console.log('server started ' + port)
