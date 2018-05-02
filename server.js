a853ce605f3bb5b4ad7f5e8401dcadeced454c7d
Ron Fridman <fridmanron@gmail.com>
2018-05-02 16:43:41 +0300

    chat bot update

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 // respond with "hello world" when a GET request is made to the homepage
-// app.use(serveStatic(path.join(__dirname, 'dist')))
-app.use('*', express.static('index.html'))
+app.use(serveStatic(path.join(__dirname, 'chatbot')))
+app.use('*', express.static('chatbot/index.html'))
 var port = process.env.PORT || 5000
 
 app.listen(port)
